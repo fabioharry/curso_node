59b8e5081f7b7ef3c8271c86b76523d54574b430
fabioharry <38741153+fabioharry@users.noreply.github.com>
2018-05-08 22:48:26 -0300

    Adição de validação no formulário

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,32 @@
 module.exports = function(application){
 
     application.get('/formulario_inclusao_noticia', function(req, res){
-      res.render("admin/form_add_noticia")
+      res.render("admin/form_add_noticia", {validacao: {}, noticia: {}})
     })
+
+
 
     application.post('/noticias/salvar', function(req, res){
      // res.render("admin/form_add_noticia")
      var noticia = req.body
-    
+
+     console.log(noticia)
+     //Validação de campos fomulário
+     req.assert('titulo', 'Título é obrigatório').notEmpty()
+     req.assert('resumo','Resumo é obrigatório').notEmpty()
+     req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100)
+     req.assert('autor','Autor é obrigatório').notEmpty()
+    //req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'})
+     req.assert('data_noticia', 'Data').notEmpty().isISO8601({format: 'YYYY-MM-DD'})
+     req.assert('noticia','Noticia é obrigatório').notEmpty()
+
+      var erros = req.validationErrors()
+      console.log(erros)
+
+      if(erros){
+        res.render("admin/form_add_noticia", {validacao : erros, noticia : noticia})
+        return
+      }
 
       // Conexão
       var connection = application.config.dbConnection()
@@ -20,9 +39,17 @@ module.exports = function(application){
         res.redirect('/noticias')
     })
 
-    
+    // function isValidDate(value) {
+    //   if (!value.match(/^\d{4}-\d{2}-\d{2}$/)) return false;
+    //   const date = new Date(value);
+    //   if (!date.getTime()) return false;
+    //   return date.toISOString().slice(0, 10) === value;
+    //   }
 
     })
-
+    
 
 }
+
+
+  
