b991e0298aea8f27592b1bf916718996ad68de19
fabioharry <38741153+fabioharry@users.noreply.github.com>
2018-05-10 22:44:07 -0300

    Adição de dinamica nas views e captura de parametros via url

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -12,9 +12,11 @@ module.exports.noticia = function(application, req, res){
     var connection = application.config.dbConnection()
     var noticiasModel = new application.app.models.NoticiasDAO(connection)
     
+    // Estamos recuperando os paramentros passados pela URL
+    var id_noticia = req.query
 
     // fazendo uma consulta
-    noticiasModel.getNoticia(function(error, result){
+    noticiasModel.getNoticia(id_noticia, function(error, result){
         res.render("noticias/noticia", {noticia : result})
     })
 }
